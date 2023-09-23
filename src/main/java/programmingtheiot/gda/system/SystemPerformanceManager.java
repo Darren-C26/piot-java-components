32fb620112a3f3ea2f41e566e4df220338aa2420
Darren-C26 <decons2602@gmail.com>
2023-09-22 22:01:11 -0400

    Update SystemPerformanceManager.java

--- src/main/java/programmingtheiot/gda/system/SystemPerformanceManager.java
+++ src/main/java/programmingtheiot/gda/system/SystemPerformanceManager.java
@@ -18,6 +18,8 @@ import programmingtheiot.common.ConfigUtil;
 import programmingtheiot.common.IDataMessageListener;
 import programmingtheiot.common.ResourceNameEnum;
 import programmingtheiot.data.SystemPerformanceData;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 /**
  * Shell representation of class for student implementation.
@@ -26,7 +28,15 @@ import programmingtheiot.data.SystemPerformanceData;
 public class SystemPerformanceManager
 {
 	// private var's
+	private static final Logger _Logger = Logger.getLogger(SystemPerformanceManager.class.getName());
+	private int pollRate = ConfigConst.DEFAULT_POLL_CYCLES;
 	
+	private ScheduledExecutorService schedExecSvc = null;
+	private SystemCpuUtilTask sysCpuUtilTask = null;
+	private SystemMemUtilTask sysMemUtilTask = null;
+
+	private Runnable taskRunner = null;
+	private boolean isStarted = false;
 	
 	// constructors
 	
@@ -36,6 +46,20 @@ public class SystemPerformanceManager
 	 */
 	public SystemPerformanceManager()
 	{
+		this.pollRate =
+			ConfigUtil.getInstance().getInteger(
+				ConfigConst.GATEWAY_DEVICE, ConfigConst.POLL_CYCLES_KEY, ConfigConst.DEFAULT_POLL_CYCLES);
+		
+		if (this.pollRate <= 0) {
+			this.pollRate = ConfigConst.DEFAULT_POLL_CYCLES;
+		}
+		
+		this.schedExecSvc   = Executors.newScheduledThreadPool(1);
+		this.sysCpuUtilTask = new SystemCpuUtilTask();
+		this.sysMemUtilTask = new SystemMemUtilTask();
+		this.taskRunner = () -> {
+			this.handleTelemetry();
+		};
 	}
 	
 	
@@ -43,18 +67,39 @@ public class SystemPerformanceManager
 	
 	public void handleTelemetry()
 	{
+		float cpuUtil = this.sysCpuUtilTask.getTelemetryValue();
+		float memUtil = this.sysMemUtilTask.getTelemetryValue();
+		
+		_Logger.info("CPU utilization: " + cpuUtil + ", Mem utilization: " + memUtil);
 	}
 	
 	public void setDataMessageListener(IDataMessageListener listener)
 	{
 	}
 	
-	public void startManager()
+	public boolean startManager()
 	{
+		if (! this.isStarted) {
+			_Logger.info("SystemPerformanceManager is starting...");
+			
+			ScheduledFuture<?> futureTask =
+				this.schedExecSvc.scheduleAtFixedRate(this.taskRunner, 1L, this.pollRate, TimeUnit.SECONDS);
+			this.isStarted = true;
+		} else {
+			_Logger.info("SystemPerformanceManager is already started.");
+		}
+		
+		return this.isStarted;
 	}
-	
-	public void stopManager()
+
+	public boolean stopManager()
 	{
+		this.schedExecSvc.shutdown();
+		this.isStarted = false;
+		
+		_Logger.info("SystemPerformanceManager is stopped.");
+		
+		return true;
 	}
 	
 }
