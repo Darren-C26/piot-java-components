785223c745795072526d660e42fe2c21fc0ba935
Darren-C26 <decons2602@gmail.com>
2023-10-06 23:09:54 -0400

    Update SensorData.java

--- src/main/java/programmingtheiot/data/SensorData.java
+++ src/main/java/programmingtheiot/data/SensorData.java
@@ -22,7 +22,7 @@ public class SensorData extends BaseIotData implements Serializable
 	
 	
 	// private var's
-	
+	private float value = ConfigConst.DEFAULT_VAL;
     
 	// constructors
 	
@@ -41,11 +41,14 @@ public class SensorData extends BaseIotData implements Serializable
 	
 	public float getValue()
 	{
-		return 0.0f;
+		return value;
 	}
 	
 	public void setValue(float val)
 	{
+		super.updateTimeStamp();
+
+		this.value = val;
 	}
 	
 	/**
@@ -72,6 +75,10 @@ public class SensorData extends BaseIotData implements Serializable
 	 */
 	protected void handleUpdateData(BaseIotData data)
 	{
+		 if (data instanceof SensorData) {
+			 SensorData sData = (SensorData) data;
+			 this.setValue(sData.getValue());
+			 }		
 	}
 	
 }
