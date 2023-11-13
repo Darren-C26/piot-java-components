37c339c0dd27dfb15db53b600629db280264d463
Darren-C26 <decons2602@gmail.com>
2023-11-13 11:38:58 -0500

    Update GetActuatorCommandResourceHandler.java

--- src/main/java/programmingtheiot/gda/connection/handlers/GetActuatorCommandResourceHandler.java
+++ src/main/java/programmingtheiot/gda/connection/handlers/GetActuatorCommandResourceHandler.java
@@ -8,6 +8,7 @@
 
 package programmingtheiot.gda.connection.handlers;
 
+
 import java.util.logging.Logger;
 
 import org.eclipse.californium.core.CoapResource;
@@ -22,63 +23,61 @@ import programmingtheiot.common.ResourceNameEnum;
 import programmingtheiot.data.ActuatorData;
 import programmingtheiot.data.DataUtil;
 
-
-/**
- * Shell representation of class for student implementation.
- *
- */
-public class GetActuatorCommandResourceHandler extends CoapResource	implements IActuatorDataListener {
+public class GetActuatorCommandResourceHandler extends CoapResource
+implements IActuatorDataListener {
+	
 	// static
 	
-	// logging infrastructure - should already be defined, although you'll need
-	// to update the class name as shown below
-	private static final Logger _Logger =
-		Logger.getLogger(GetActuatorCommandResourceHandler.class.getName());
-	
-	// params
-	
-	private ActuatorData actuatorData = null;
-	
-	// constructors
-	public GetActuatorCommandResourceHandler(String resourceName)
-	{
-		super(resourceName);
+		// logging infrastructure - should already be defined, although you'll need
+		// to update the class name as shown below
+		private static final Logger _Logger =
+			Logger.getLogger(GetActuatorCommandResourceHandler.class.getName());
+		
+		// params
+		
+		private ActuatorData actuatorData = null;
+		
+		// constructors
+		
+		public GetActuatorCommandResourceHandler(String resourceName)
+		{
+			super(resourceName);
 
-		// set the resource to be observable
-		super.setObservable(true);
-	}
-	
-	public boolean onActuatorDataUpdate(ActuatorData data)
-	{
-		if (data != null && this.actuatorData != null) {
-			this.actuatorData.updateData(data);
+			// set the resource to be observable
+			super.setObservable(true);
+		}
+
+		public boolean onActuatorDataUpdate(ActuatorData data)
+		{
+			if (data != null && this.actuatorData != null) {
+				this.actuatorData.updateData(data);
+				
+				// notify all connected clients
+				super.changed();
+				
+				_Logger.fine("Actuator data updated for URI: " + super.getURI() + ": Data value = " + this.actuatorData.getValue());
+				
+				return true;
+			}
 			
-			// notify all connected clients
-			super.changed();
-			
-			_Logger.fine("Actuator data updated for URI: " + super.getURI() + ": Data value = " + this.actuatorData.getValue());
-			
-			return true;
+			return false;
 		}
 		
-		return false;
-	}
+		@Override
+		public void handleGET(CoapExchange context)
+		{
+			// TODO: validate 'context'
+		    
+		    // accept the request
+		    context.accept();
+		    
+		    // TODO: convert the locally stored ActuatorData to JSON using DataUtil
+		    String jsonData = DataUtil.getInstance().actuatorDataToJson(this.actuatorData);
 
-	@Override
-	public void handleGET(CoapExchange context)
-	{
-	    // TODO: validate 'context'
-	    
-	    // accept the request
-	    context.accept();
-	    
-	    // TODO: convert the locally stored ActuatorData to JSON using DataUtil
-	    String jsonData = DataUtil.getInstance().actuatorDataToJson(this.actuatorData);
+		    // TODO: generate a response message, set the content type, and set the response code
 
-	    // TODO: generate a response message, set the content type, and set the response code
+		    // send an appropriate response
+		    context.respond(ResponseCode.CONTENT, jsonData);
+		}
 
-	    // send an appropriate response
-	    context.respond(ResponseCode.CONTENT, jsonData);
-	}
-	
 }
