fd334903921c3c11d9d7a547bca4e5cd5c59f17e
Darren-C26 <decons2602@gmail.com>
2023-11-17 20:20:23 -0500

    Update SystemPerformanceManager.java

--- src/main/java/programmingtheiot/gda/system/SystemPerformanceManager.java
+++ src/main/java/programmingtheiot/gda/system/SystemPerformanceManager.java
@@ -18,6 +18,7 @@ import programmingtheiot.common.ConfigUtil;
 import programmingtheiot.common.IDataMessageListener;
 import programmingtheiot.common.ResourceNameEnum;
 import programmingtheiot.data.SystemPerformanceData;
+
 import java.util.logging.Logger;
 import java.util.logging.Level;
 
@@ -31,28 +32,25 @@ public class SystemPerformanceManager
 	private static final Logger _Logger = Logger.getLogger(SystemPerformanceManager.class.getName());
 	private int pollRate = ConfigConst.DEFAULT_POLL_CYCLES;
 	
-	private ScheduledExecutorService schedExecSvc = null;
+	private ScheduledExecutorService schedExecSvc = null; //labmodule2
 	private SystemCpuUtilTask sysCpuUtilTask = null;
 	private SystemMemUtilTask sysMemUtilTask = null;
 
 	private Runnable taskRunner = null;
 	private boolean isStarted = false;
 	
-	private String locationID = ConfigConst.NOT_SET;
+	private String locationID = ConfigConst.NOT_SET; //Lab module 05
 	private IDataMessageListener dataMsgListener = null;
-	
+	private SystemDiskUtilTask sysDiskUtilTask = null;
 	// constructors
 	
 	/**
 	 * Default.
 	 * 
 	 */
-	public SystemPerformanceManager()
-	{
-		this.pollRate =
-			ConfigUtil.getInstance().getInteger(
-				ConfigConst.GATEWAY_DEVICE, ConfigConst.POLL_CYCLES_KEY, ConfigConst.DEFAULT_POLL_CYCLES);
-		
+	public SystemPerformanceManager(){
+		this.pollRate = ConfigUtil.getInstance().getInteger(ConfigConst.GATEWAY_DEVICE, ConfigConst.POLL_CYCLES_KEY, ConfigConst.DEFAULT_POLL_CYCLES);
+			
 		if (this.pollRate <= 0) {
 			this.pollRate = ConfigConst.DEFAULT_POLL_CYCLES;
 		}
@@ -60,66 +58,60 @@ public class SystemPerformanceManager
 		this.schedExecSvc   = Executors.newScheduledThreadPool(1);
 		this.sysCpuUtilTask = new SystemCpuUtilTask();
 		this.sysMemUtilTask = new SystemMemUtilTask();
+		this.sysDiskUtilTask = new SystemDiskUtilTask();
+		this.locationID = ConfigUtil.getInstance().getProperty(ConfigConst.GATEWAY_DEVICE, ConfigConst.LOCATION_ID_PROP, ConfigConst.NOT_SET);
+		
 		this.taskRunner = () -> {
 			this.handleTelemetry();
 		};
-		
-		this.locationID =
-				ConfigUtil.getInstance().getProperty(
-					ConfigConst.GATEWAY_DEVICE, ConfigConst.LOCATION_ID_PROP, ConfigConst.NOT_SET);
 	}
 	
 	
 	// public methods
 	
-	public void handleTelemetry()
-	{
+	public void handleTelemetry(){
 		float cpuUtil = this.sysCpuUtilTask.getTelemetryValue();
 		float memUtil = this.sysMemUtilTask.getTelemetryValue();
+		float diskUtil = this.sysDiskUtilTask.getTelemetryValue();
 		
-		// TODO: change the log level to 'info' for testing purposes
-		_Logger.info("CPU utilization: " + cpuUtil + ", Mem utilization: " + memUtil);
+		_Logger.info("CPU utilization: " + cpuUtil + ", Mem utilization: " + memUtil +", Disk utilization: "+ diskUtil);
 		
 		SystemPerformanceData spd = new SystemPerformanceData();
 		spd.setLocationID(this.locationID);
 		spd.setCpuUtilization(cpuUtil);
 		spd.setMemoryUtilization(memUtil);
+		spd.setDiskUtilization(diskUtil);
 		
 		if (this.dataMsgListener != null) {
-			this.dataMsgListener.handleSystemPerformanceMessage(
-				ResourceNameEnum.GDA_SYSTEM_PERF_MSG_RESOURCE, spd);
+			this.dataMsgListener.handleSystemPerformanceMessage(ResourceNameEnum.GDA_SYSTEM_PERF_MSG_RESOURCE, spd);
 		}
 	}
 	
-	public void setDataMessageListener(IDataMessageListener listener)
-	{
+	public void setDataMessageListener(IDataMessageListener listener){
 		if (listener != null) {
 			this.dataMsgListener = listener;
 		}
 	}
 	
-	public boolean startManager()
-	{
-		if (! this.isStarted) {
+	public boolean startManager(){
+		if (!this.isStarted){
 			_Logger.info("SystemPerformanceManager is starting...");
 			
-			ScheduledFuture<?> futureTask =
-				this.schedExecSvc.scheduleAtFixedRate(this.taskRunner, 1L, this.pollRate, TimeUnit.SECONDS);
+			ScheduledFuture<?> futureTask = this.schedExecSvc.scheduleAtFixedRate(this.taskRunner, 1L, this.pollRate, TimeUnit.SECONDS);
+			
 			this.isStarted = true;
-		} else {
+		}else {
 			_Logger.info("SystemPerformanceManager is already started.");
 		}
 		
 		return this.isStarted;
 	}
 
-	public boolean stopManager()
-	{
+	public boolean stopManager(){
 		this.schedExecSvc.shutdown();
 		this.isStarted = false;
 		
 		_Logger.info("SystemPerformanceManager is stopped.");
-		
 		return true;
 	}
 	
